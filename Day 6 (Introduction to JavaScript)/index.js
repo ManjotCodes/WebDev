9841c872fecd47952df4fa177937e636d5902360
Manjot Singh <110255794+ManjotCodes@users.noreply.github.com>
2022-11-06 23:15:40 +0530

    Add files via upload

---
+++ Day 6 (Introduction to JavaScript)/index.js
@@ -0,0 +1,22 @@
+// // hmar pehla js code
+// console.log('Hello World');
+// let name = 'Manjot';
+// console.log(name);
+// // Rules for variable declaration
+// // Cannot be a reserved keyword
+// // Should be meaningful
+// // Cannot start with a number (1name)
+
+// // 
+
+let person={
+    name: 'Manjot',
+    age: 30
+};
+console.log(person.name +"\n "+ person.age);
+// person.name='Aman'
+// console.log(person.name);
+// person.age= 6
+// console.log(person.age);
+
+
